08bd814343b49a6ea684bbeeef1394389e2b4f66
wuhuanglong <2657332467@qq.com>
2017-07-03 14:53:12 +0800

    请指定提交信息

---
+++ scalers python.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env  python
+#-*- coding:utf-8  -*-
+def convertCookieStrToDict(cookieStr):
+    StrArray = CookieStr.split(";")
+    tempDict = {}
+    for i in StrArray:
+        iArray =i.split("=")
+        tempDict[iArray[0]] = iArray[1]
+        return tempDict
